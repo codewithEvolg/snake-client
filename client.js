d502a6009013d93ab41d3a3b2cbcab07086936d1
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-05-22 23:46:50 -0400

    export connect in client.js file for play.js

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+// establishes a connection with the game server
+const connect = () => {
+  const conn = net.createConnection(
+    connectionListener= { 
+      host: '10.0.2.15',
+      port: '50541'// PORT number here
+    }
+  );
+  conn.on("connect", (data) => {
+    // code that does something when the connection is first established
+    console.log('Hello');
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect;
